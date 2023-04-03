a23e9214875f34637d2c6c0c031c17e5ae1b6b3d
JohnWick1K47 <69172674+JohnWick1K47@users.noreply.github.com>
2023-04-04 00:13:39 +0530

    Update chidori.py

--- chidori.py
+++ chidori.py
@@ -4,10 +4,10 @@ import asyncio
 async def naruto(ult):
 	await ult.edit("Hello")
 	await asyncio.sleep(1)
-	await ult.edit("Im John Wick You May Know Me As Copy John ")
-	await asyncio.sleep(3)
+	await ult.edit("I'm John Wick You May Know Me As Baba Yaga☠️")
+	await asyncio.sleep(4)
 	await ult.edit("You Are Gonna Pay For What You Did To My Comrades")
-	await asyncio.sleep(2)
+	await asyncio.sleep(3)
 	await ult.edit("( ◗_ ╂ ) ☞✹)@JohnWick1K47")
 	await asyncio.sleep(1)
 	await ult.edit("You:(✖﹏✖)")
