537b8ce4469f91e1d2ffe91a03be1567cd43f94c
JohnWick1K47 <69172674+JohnWick1K47@users.noreply.github.com>
2023-04-04 00:09:23 +0530

    Update chidori.py

--- chidori.py
+++ chidori.py
@@ -1,14 +1,14 @@
 import asyncio
 #Credit To @senku_ishigamiii/@uzumaki_naruto_xd
-@ultroid_cmd(pattern="chidori")
+@ultroid_cmd(pattern="me")
 async def naruto(ult):
 	await ult.edit("Hello")
 	await asyncio.sleep(1)
-	await ult.edit("Im Kakashi You May Know Me As Copy Ninja ")
+	await ult.edit("Im John Wick You May Know Me As Copy John ")
 	await asyncio.sleep(3)
 	await ult.edit("You Are Gonna Pay For What You Did To My Comrades")
 	await asyncio.sleep(2)
-	await ult.edit("( ◗_ ╂ ) ☞✹)Chidori ")
+	await ult.edit("( ◗_ ╂ ) ☞✹)@JohnWick1K47")
 	await asyncio.sleep(1)
 	await ult.edit("You:(✖﹏✖)")
 	
